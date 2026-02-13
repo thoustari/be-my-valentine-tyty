6681d5ab3aeae8c9c441ffc43c4c08694a9f1337
thoustari <arianaplacson@gmail.com>
2026-02-13 15:03:12 -0800

    Update config.js

--- config.js
+++ config.js
@@ -43,8 +43,8 @@ const CONFIG = {
     // They show up depending on how far they slide the meter
     loveMessages: {
         extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        high: "Forever & ALWAYSSS! 🚀💝",              // Shows when they go past 1000%
+        normal: "& more!! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
